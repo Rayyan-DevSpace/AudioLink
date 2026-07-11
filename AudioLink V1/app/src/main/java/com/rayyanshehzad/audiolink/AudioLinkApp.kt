4c450b5fd565d79cdd63ee107f7ceb812e044f85
Muhammad Rayyan <mrayyanshehzad@gmail.com>
2026-07-11 23:34:16 +0500

    nothing

--- AudioLink V1/app/src/main/java/com/rayyanshehzad/audiolink/AudioLinkApp.kt
+++
@@ -1,8 +0,0 @@
-package com.rayyanshehzad.audiolink
-
-import android.app.Application
-import com.rayyanshehzad.audiolink.data.RoutingRepository
-
-class AudioLinkApp : Application() {
-    val repository: RoutingRepository by lazy { RoutingRepository(this) }
-}
